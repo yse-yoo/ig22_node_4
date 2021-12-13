a7ef1b4ce63900e3ca2e0776487ef8983304251e
yse-yoo <youhei_yoshikawa@yse-c.net>
2021-12-13 10:34:04 +0900

    add io socket

--- server.js
+++ server.js
@@ -8,13 +8,21 @@ const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 
 const dotenv = require('dotenv')
-
 dotenv.config()
 const host = process.env.HOST
 const port = process.env.PORT
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(__dirname + '/public'))
+
+//接続（せつぞく）されたら実行（じっこう）
+io.on('connection', (socket) => {
+    socket.on('message', (data) => {
+        console.log(data)
+        io.emit('message', data)
+    })
+})
+
 
 http.listen(port, host, () => {
     console.log('http://' + host + ':' + port)
